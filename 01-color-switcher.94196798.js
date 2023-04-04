4b4d2f52559cde702cc924fb6c4c0809435674f1
Vallyblink <115736687+Vallyblink@users.noreply.github.com>
2023-04-04 12:59:14 +0000

    Deploying to gh-pages from @ Vallyblink/goit-js-hw-09@347d9f6fcd7a19e05f0526ecfc6c716e8dc88d50 🚀

---
+++ 01-color-switcher.94196798.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a;t.addEventListener("click",(function(){a=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(a,1e3),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.94196798.js.map
